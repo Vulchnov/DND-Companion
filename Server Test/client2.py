471378a0d73c642c82bfdd7658d97888e293461f
Vulchnov <92955833+Vulchnov@users.noreply.github.com>
2025-03-22 14:26:20 -0600

    Update client2.py

--- Server Test/client2.py
+++ Server Test/client2.py
@@ -6,6 +6,7 @@ BROADCAST_IP = "255.255.255.255"  # Broadcast address for local network
 # Create UDP socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+client_socket.bind(('', BROADCAST_PORT))
 
 message = "Hello, any servers out there?"
 client_socket.sendto(message.encode(), ("<broadcast>", BROADCAST_PORT))
